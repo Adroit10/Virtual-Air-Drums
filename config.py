006f48e6c18657fcbf668acf1fa6d2f525271d7a
Adroit10 <tripathidevansh7@gmail.com>
2026-01-19 22:22:44 +0530

    config changes

--- config.py
+++ config.py
@@ -1,25 +1,19 @@
 import numpy as np
 
-# Camera
 CAMERA_INDEX = 0
 
-# HSV color range for the stick tip (example: green)
-# You will fine-tune these after testing
 LOWER_COLOR = np.array([35, 70, 70])
 UPPER_COLOR = np.array([85, 255, 255])
 
-# Minimum contour area to be considered as stick
 MIN_AREA = 30
 
-# Drum Zones (x, y, width, height)
+# x,y,w,h
 DRUMS = {
     "snare": (100, 300, 150, 150),
     "kick":  (300, 300, 150, 150),
     "hihat": (500, 300, 150, 150)
 }
 
-# Hit control
-HIT_DELAY = 0.2  # seconds between two hits on same drum
+HIT_DELAY = 0.2 
 
-# Speed to volume conversion
-MAX_SPEED = 1000  # tune later
+MAX_SPEED = 1000 
